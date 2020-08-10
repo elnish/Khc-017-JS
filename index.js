a248562f41a909881ecd5c63f1f3445987e8a2c4
elnish <you@example.com>
2020-08-10 19:49:51 +0300

    finished

---
+++ index.js
@@ -0,0 +1,77 @@
+/* Подключение */
+alert("Alena"); 
+
+/* Переменные */
+/* 1 */
+var title;
+title = "Variables";
+console.log(title);
+title = "Second title";
+console.log(title);
+
+/* 2 */
+var a = 25, b =10;
+console.log('a = ' + a, 'b = ' + b);
+
+a = b;
+console.log('a = ' + a, 'b = ' + b);
+
+/* Взаимодействие с пользователем */
+/* var  someName = prompt('Enter your name', '');
+alert(someName); */
+
+/* Операции с переменными */
+/* 1 */
+var c = 5, s;
+s = c * c;
+console.log(c);
+
+/* 2 */
+var number = 48;
+var k = number / 12;
+var g = number%40;
+console.log('namber = ' + number, "десятки: " + k, "единицы: " + g);
+
+/* 3 */
+var d = 2, e = 5, f = 1;
+console.log("d > 2 ? " + d > 2);
+console.log("e <= 3 ? " + e <= 3);
+
+/* 4 */
+console.log("d < e < f ? " +  d < e < f);
+
+/* 5 */
+var j = 101;
+if ( j % 2 !== 0 && j > 100 ) {
+	console.log('данное число является нечетным трехзначным.');
+} 
+
+/* Преобразование данных */
+var x = 1;
+var y = 2;
+
+var res1 = String(x) + y;
+console.log(res1);
+console.log(typeof res1); 
+
+var res2 = Boolean(x) + String(y);
+console.log(res2); 
+console.log(typeof res2); 
+var res3 = Boolean(x + y);
+console.log(res3); 
+console.log(typeof res3); 
+var res4 = Number(x) - 'y';
+console.log(res4); 
+console.log(typeof res4);
+
+/* Array */
+var arr = [];
+arr[0] = 12;
+arr[1] = "string";
+arr[2] = true;
+arr[3] = null;
+console.log("Length = " + arr.length);
+
+var z = Number(prompt('Enter the number', ''));
+arr[4] = z;
+console.log(arr[4]);
