5767b3d569ef4c44405e904e7466acb3803fb94d
elnish <you@example.com>
2020-08-31 12:51:53 +0300

    finished objects in function

---
+++ Объекты в функциях/index.js
@@ -0,0 +1,71 @@
+"use strict";
+
+// #1
+
+function createMP3(model, memory) {
+  return {
+    model: model,
+    memory: memory,
+    play: false,
+    describe: function() {
+      console.log('MP3 фирмы ' + this.model + ',  встроенная память ' + this.memory + 'GB');
+    },
+    ternOn: function() {
+      this.play == true;
+      console.log('play');
+    },
+    ternOff: function() {
+      this.play == false;
+      console.log('stop');
+    }
+  
+  }
+};
+
+var sony = createMP3('Sony', 4);
+sony.describe();
+sony.ternOn();
+sony.ternOff();
+
+// #2
+
+function getArithmeticMean(obj) {
+  return {
+    object: obj,
+    sum: function() {
+      var sum = 0;
+      var items = 0;
+      for (var key in this.object) {
+        sum += this.object[key];
+        items++;
+      }
+      return Math.round(sum / items);
+    }
+  }
+}
+
+
+var salaries = {
+  John: 100,
+  Bill: 300,
+  Mike: 250
+};
+
+var employees = getArithmeticMean(salaries);
+
+console.log( employees.sum() );
+
+function sum(obj) {
+  var sum = 0;
+  var items = 0;
+  for (var key in obj) {
+     sum += obj[key];
+     items++;
+
+  }
+  return Math.round(sum / items);
+}
+
+console.log( sum(salaries) );
+
+
